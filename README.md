7564f1a3e200152d178997b2518480f458d06c04
MrozKarol <polityk357@gmail.com>
2022-10-30 13:39:09 +0100

    implementation of vue, technology comparison

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # vue_aventure
+
+01_Geeting Startet : building a add goal project using html and js, then
+implementation of vue,
+technology comparison
--- vue_basic/01_Geeting Startet/app.js
+++ vue_basic/01_Geeting Startet/app.js
@@ -1,13 +1,28 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredVale: '',
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredVale);
+      this.enteredVale = '';
+    },
+  },
+}).mount('#app');
 
-function addGoal() {
-  const inputVale = inputEl.value;
-  const newLi = document.createElement('li');
-  newLi.textContent = inputVale;
-  listEl.appendChild(newLi);
-  inputEl.value = '';
-}
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-buttonEl.addEventListener('click', addGoal);
+// function addGoal() {
+//   const inputVale = inputEl.value;
+//   const newLi = document.createElement('li');
+//   newLi.textContent = inputVale;
+//   listEl.appendChild(newLi);
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
