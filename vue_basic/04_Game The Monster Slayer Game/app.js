e5d6b0c347346dc682e0ba992b73f4b5cea3f413
MrozKarol <polityk357@gmail.com>
2022-11-15 17:07:46 +0100

    04_Game The Monster Slayer Game - add heal player

--- vue_basic/04_Game The Monster Slayer Game/app.js
+++ vue_basic/04_Game The Monster Slayer Game/app.js
@@ -40,6 +40,15 @@ const app = Vue.createApp({
       this.monsterHealth -= attackValue;
       this.attackPlayer();
     },
+    healPlayer() {
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
   },
 });
 
