1b1fa11e896b6879c05d16d4d9f7c0e38f318dc3
MrozKarol <polityk357@gmail.com>
2022-11-18 17:34:08 +0100

    04_Game The Monster Slayer Game - Adding a Battle Log

--- vue_basic/04_Game The Monster Slayer Game/app.js
+++ vue_basic/04_Game The Monster Slayer Game/app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
       monsterHealth: 100,
       currenrRound: 0,
       winner: null,
+      logMessages: [],
     };
   },
   computed: {
@@ -58,23 +59,27 @@ const app = Vue.createApp({
       this.monsterHealth = 100;
       this.currenrRound = 0;
       this.winner = null;
+      this.logMessages = [];
     },
 
     attackMonster() {
       this.currenrRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue);
       this.attackPlayer();
       console.log(this.playerHealth, this.monsterHealth);
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage('monster', 'attack', attackValue);
     },
     specialAttackMonster() {
       this.currenrRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue);
       this.attackPlayer();
     },
     healPlayer() {
@@ -84,7 +89,15 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue;
       }
+      this.addLogMessage('player', 'heal', healValue);
       this.attackPlayer();
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
 });
