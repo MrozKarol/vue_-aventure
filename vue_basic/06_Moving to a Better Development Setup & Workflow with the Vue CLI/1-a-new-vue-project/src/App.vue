e9c53d8ef494d00cdabf14c4a885120389f71bd8
MrozKarol <polityk357@gmail.com>
2022-12-07 17:10:07 +0100

    Creating a Basic Vue App

---
+++ vue_basic/06_Moving to a Better Development Setup & Workflow with the Vue CLI/1-a-new-vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+<h2>My Freinds</h2>
+<ul>
+    <li></li>
+</ul>
+    </section>
+</template>
+
+
+<script>
+export default{
+    data(){
+        return{
+            frainds: [
+        {
+          id: 'janek',
+          name: 'Janek Ogarek',
+          phone: '0800 000 000',
+          emial: 'janek@hotmail.coom',
+        },
+        {
+          id: 'milena',
+          name: 'Milena Majonez',
+          phone: '0700 555 000',
+          emial: 'milena@hotmail.coom',
+        },
+      ],
+        }
+    
+    }
+    }
+</script>
