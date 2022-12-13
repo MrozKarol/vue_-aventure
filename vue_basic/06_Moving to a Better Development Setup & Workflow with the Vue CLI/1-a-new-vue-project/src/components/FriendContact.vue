db984683fb70fcf773e7193b53ad40d6379aff10
MrozKarol <polityk357@gmail.com>
2022-12-13 17:32:30 +0100

    Creating a Basic Vue App-Adding a Component

---
+++ vue_basic/06_Moving to a Better Development Setup & Workflow with the Vue CLI/1-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <h2>{{ fraind.name }}</h2>
+    <button @click="toggleDetails">Show Detalis</button>
+    <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong>{{ fraind.phone }}</li>
+        <li><strong>Email:</strong>{{ fraind.emial }}</li>
+    </ul>
+
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            fraind:
+            {
+                id: 'janek',
+                name: 'Janek Ogarek',
+                phone: '0800 000 000',
+                emial: 'janek@hotmail.coom',
+            },
+
+        }
+
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible
+        }
+    }
+}
+</script>
